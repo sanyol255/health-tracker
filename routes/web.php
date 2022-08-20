1e71604c88ec9191b65eb5c63789990b53bdb05f
Oleksandr Kovalchuk <oleksandr.kovalchuk.dev89@gmail.com>
2022-08-20 17:34:37 +0300

    Adding main page and basic style

--- routes/web.php
+++ routes/web.php
@@ -13,14 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/test', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::view('/', 'layouts.main-page');
